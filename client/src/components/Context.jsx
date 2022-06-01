bee5b93c7e45cead948dcc3fc055e69cd06985fc
Igor <machold.igor@gmail.com>
2022-06-01 16:14:06 +0200

    commit

--- client/src/components/Context.jsx
+++ client/src/components/Context.jsx
@@ -3,64 +3,61 @@ import axios from "axios";
 
 export const MyContext = createContext();
 export default function MyContextProvider({ children }) {
-    const [cart,setCart] = useState([])
+  const [cart, setCart] = useState([]);
 
-    const addToCart = item => {
+  const addToCart = (item) => {
+    const exist = cart.find((x) => x.id === item.id);
+    if (exist) {
+      setCart(
+        cart.map((x) =>
+          x.id === item.id ? { ...exist, qty: exist.qty + 1 } : x
+        )
+      );
+    } else {
+      setCart([...cart, { ...item, qty: 1 }]);
+    }
+  }; // adds items to the cart
 
-      const exist = cart.find(x => x.id === item.id) 
-      if(exist){
-        setCart(cart.map(x => x.id === item.id? {...exist, qty: exist.qty +1}: x))
-      } else {
-        setCart([...cart, {...item, qty:1}])
-      }
+  const removeItem = (item) => {
+    const exist = cart.find((x) => x.id === item.id);
+    if (exist.qty === 1) {
+      setCart(cart.filter((x) => x.id !== item.id));
+    } else {
+      setCart(
+        cart.map((x) =>
+          x.id === item.id ? { ...exist, qty: exist.qty - 1 } : x
+        )
+      );
+    }
+  };
+
+  const itemsPrice = cart.reduce((a,c) => a + c.price * c.price, 0)
 
 
-       
-      
-      } // adds items to the cart
 
-
-   
-
-      const removeItem = (item) => {
-        const exist = cart.find((x) => x.id === item.id)
-        if(exist.qty === 1){
-          setCart(cart.filter((x) => x.id !== item.id))
-
-        } else {
-          setCart(cart.map(x => x.id === item.id? {...exist, qty: exist.qty - 1}: x))
-
-
-        }
-
-      }
-    
-        
-        
-      // const addItem = () => {
-      //   const exist = cart.find(x => x.id === item.id) 
-      //   if(exist) {
-  
-      //     setCart(cart.map(x => x.id === item.id ? {...exist, qty : exist.qty +1 } : x))
-
-      // }  else {
-      //   setCart([...cart, {...item, qty:1}])
-      // }
-    
   const [currentUser, setCurrentUser] = useState("");
   const [userObject, setUserObject] = useState();
 
-//   useEffect(() => {
-//     axios.get("/getuser", { withCredentials: true }).then((res) => {
-//       console.log(res);
-//       if (res.data) {
-//         setUserObject(res.data);
-//       }
-//     });
-//   }, []);
+  //   useEffect(() => {
+  //     axios.get("/getuser", { withCredentials: true }).then((res) => {
+  //       console.log(res);
+  //       if (res.data) {
+  //         setUserObject(res.data);
+  //       }
+  //     });
+  //   }, []);
 
   return (
-    <MyContext.Provider value={{ userObject, currentUser, setCurrentUser,addToCart,cart,removeItem }}>
+    <MyContext.Provider
+      value={{
+        userObject,
+        currentUser,
+        setCurrentUser,
+        addToCart,
+        cart,
+        removeItem,
+      }}
+    >
       {children}
     </MyContext.Provider>
   );
